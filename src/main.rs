26df1e2ec8cc6b12cebb043113c594272f34c9f6
Stephen Burwell <Stephen@Meghans-MacBook-Air.local>
2026-03-23 11:10:16 -0400

    Done for now. I've run out of time. Particle should be moving across the screen. Not to be

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
+mod particle;
+use macroquad::prelude::*;
+use particle::*;
 
+#[macroquad::main("Fireworks!")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+        let mut particle = Particle::new(100., 100.)
+            .with_speed(Vec2 { x: 2., y: 2. })
+            .with_acceleration(Vec2 { x: 5., y: 4. });
 
-
-
-
-fn main() {
-    println!("Hello, world!");
+        particle.update();
+        particle.draw();
+        next_frame().await
+    }
 }
--- src/particle.rs
+++ src/particle.rs
@@ -1,11 +1,46 @@
+use macroquad::{color::{Color, RED}, shapes::draw_circle};
+use macroquad::prelude::*;
+use vec2::transform_mat4_projection_mut;
 
-type Vec2 = (f64, f64);
 
 
-struct Particles {
+#[derive(Debug)]
+pub struct Particle {
     position: Vec2,
-    speed:  Vec2,
+    velocity:  Vec2,
     acceleration: Vec2,
     lifetime: f64,
-    color: color
+    color: Color
+}
+
+
+impl Particle{
+   pub fn new(x: f32, y: f32) -> Particle{
+        Particle { position: Vec2 { x, y }, velocity: Vec2::default(), acceleration: Vec2::default(), lifetime: 1.0, color: Color::default()}
+    }
+
+
+    pub fn with_speed(self, velocity: Vec2) -> Particle{
+        Particle{velocity, ..self}
+    }
+
+    pub fn with_acceleration(self, acceleration: Vec2) -> Particle { 
+        Particle {acceleration, ..self}
+    }
+
+
+    pub fn update(&mut self) {
+
+        self.velocity += self.acceleration;
+        self.position += self.velocity;
+        self.acceleration = Vec2 { x: 0.0, y: 1.0 };
+        
+
+    }
+    pub fn draw(&self) {
+        draw_circle(self.position.x, self.position.y, 3.0, RED);
+        println!("{}", self.position);
+    }
+
+
 }
